e97849d63957746fa612eb2ef5c5b652b3fa53bb
Jacob Bond <jacobpbond@hotmail.co.uk>
2016-11-14 22:22:20 +0000

    lovely for some actual code....

--- data.js
+++ data.js
@@ -1,1 +1,7 @@
 
+const fs = require("fs");
+
+module.exports = {
+	load: function() {return JSON.parse(fs.readFileSync(__dirname + "/db.json"));},
+	save: function(data) {fs.writeFileSync(__dirname + "/db.json", JSON.stringify(data, null, "\t"));}
+}
