bb727af7c9c6033e9f3822fe3649d27b8914f728
Jack <epicawesomewolfgaming@gmail.com>
2016-11-15 14:20:24 -0500

    wew

--- commands/boost.js
+++ commands/boost.js
@@ -1,11 +1,13 @@
 const db = require(__dirname + '/../data.js');
+var zoom = ["zoot zoot", "WHAM!", ".....ZOOOOOOOMNMMMMMMM......", "FASTER!"]
+var z = zoom[Math.floor(Math.random() * answers.length)];
 
 module.exports.info = "placeholder.";
 module.exports.func = function(bot, m, args) {
   m.delete();
-  m.channel.sendMessage("wip")
   .then( m => {
     var num = Math.floor(Math.random() * 5000) + 100;
     db.save(db.load().guilds[m.guild.id].boost = num);
+    m.channel.sendMessage(`:rocket: | **${z}**! __${m.guild.name}__'s rocket is now at **${num} miles per hour!**`)  
   }).catch(console.error);
 }
