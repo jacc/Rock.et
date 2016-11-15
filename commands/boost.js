67fba0029feef8f9cec4a3c2c846aa51f06ca831
Jacob Bond <jacobpbond@hotmail.co.uk>
2016-11-15 19:38:08 +0000

    FIX HOPEFULLY

--- commands/boost.js
+++ commands/boost.js
@@ -1,13 +1,14 @@
 const db = require(__dirname + '/../data.js');
 var zoom = ["ZOOT ZOOT", "WHAM!", ".....ZOOOOOOOMNMMMMMMM......", "FASTER!", "WIZZ", "PEW"]
-var z = zoom[Math.floor(Math.random() * answers.length)];
 
 module.exports.info = "placeholder.";
 module.exports.func = function(bot, m, args) {
   m.delete();
   m.channel.sendMessage('wew').then( m => {
     var num = Math.floor(Math.random() * 5000) + 100;
-    db.save(db.load().guilds[m.guild.id].boost = num);
-    m.channel.sendMessage(`:rocket: | **${z}**! __${m.guild.name}__'s rocket is now at **${num} miles per hour!**`)  
+    var d = db.load();
+    d.guilds[m.guild.id].boost = num;
+    db.save(d);
+    m.channel.sendMessage(`:rocket: | **${zoom[Math.floor(Math.random() * zoom.length)]}**! __${m.guild.name}__'s rocket is now at **${num} miles per hour!**`)  
   }).catch(console.error);
 }
